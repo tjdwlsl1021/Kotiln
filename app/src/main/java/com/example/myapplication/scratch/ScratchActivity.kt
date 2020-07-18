fde3d77487b500dc7bf1ed0f64e0d25b364e9fe9
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-07-18 19:48:42 +0900

    동영상 보고 따라치기 - Scratch 복권긁기 같은기능

--- app/src/main/java/com/example/myapplication/scratch/ScratchActivity.kt
+++ app/src/main/java/com/example/myapplication/scratch/ScratchActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication.scratch
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.anupkumarpanwar.scratchview.ScratchView
 import com.example.myapplication.R
 
 class ScratchActivity : AppCompatActivity() {
@@ -9,5 +12,16 @@ class ScratchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scratch)
+
+        val scratchView = findViewById<ScratchView>(R.id.scratchView)
+        scratchView.setRevealListener(object : ScratchView.IRevealListener {
+            override fun onRevealed(scratchView: ScratchView?) {
+                Toast.makeText(applicationContext, "Reveled", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRevealPercentChangedListener(scratchView: ScratchView?, percent: Float) {
+                Log.d("scratch", percent.toString())
+            }
+        })
     }
 }
