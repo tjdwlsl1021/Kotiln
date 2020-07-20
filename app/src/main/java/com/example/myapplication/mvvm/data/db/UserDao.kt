793f396e87db2d5e9e35e8be3cc4ecca96cf4e7b
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-07-20 21:03:40 +0900

    동영상 보고 따라치기 - mvvm - roomdatabase 하는중

---
+++ app/src/main/java/com/example/myapplication/mvvm/data/db/UserDao.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.mvvm.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.myapplication.mvvm.data.db.entities.CURRENT_USER_ID
+import com.example.myapplication.mvvm.data.db.entities.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User): Long
+
+    @Query("SELETE * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser(): LiveData<User>
+}
