3ceb553c4c28f1e7c51a5e3422e0f5ac1f744a0a
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-07-20 23:16:18 +0900

    동영상 보고 따라치기 - Dagger - 패쓰

--- app/src/main/java/com/example/myapplication/dagger/BaseAppication.java
+++ app/src/main/java/com/example/myapplication/dagger/BaseAppication.java
@@ -1,14 +1,12 @@
 package com.example.myapplication.dagger;
 
-import dagger.android.AndroidInjector;
-import dagger.android.DaggerApplication;
 
-public class BaseAppication extends DaggerApplication {
-
-
-    @Override
-    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
-//        return DaggerAppComponent.builder().application(this).build(); // 너무 내용을 몰라서 모르겠다. 담기회에~
-    }
-}
+//public class BaseAppication extends DaggerApplication {
+//
+//
+//    @Override
+//    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+//        return null;
+////        return DaggerAppComponent.builder().application(this).build(); // 너무 내용을 몰라서 모르겠다. 담기회에~
+//    }
+//}
--- app/src/main/java/com/example/myapplication/dagger/DaggerActivity.kt
+++ app/src/main/java/com/example/myapplication/dagger/DaggerActivity.kt
@@ -1,13 +1,10 @@
 package com.example.myapplication.dagger
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
 
-class DaggerActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dagger)
-    }
-}
+//class DaggerActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_dagger)
+//    }
+//}
