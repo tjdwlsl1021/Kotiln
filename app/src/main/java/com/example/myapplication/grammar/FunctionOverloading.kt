ebbcb4d95bb2168338afe4bd8e8f96c85221ad6d
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-08-05 07:27:20 +0900

    동영상 보고 따라치기 - Function Overloading

---
+++ app/src/main/java/com/example/myapplication/grammar/FunctionOverloading.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.grammar
+
+fun main() {
+    println(getMax(14, 100, 9))
+}
+
+fun getMax(a: Int, b: Int) = if (a > b) a else b
+
+fun getMax(a: Double, b: Double) = if (a > b) a else b
+
+fun getMax(a: Int, b: Int, c: Int) =
+    when {
+        a >= b && a >= c -> a
+        b >= a && b >= c -> b
+        else -> c
+    }
+
+fun greet(name: String, reps: Int) {
+    for (i in 0 until reps) println("Hello, $name!")
+}
+
+fun greet(message: String, reps: Int) {
+    for (i in 0 until reps) println(message)
+}
