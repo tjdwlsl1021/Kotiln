77badfe828387276b148d672a0f2f714b98fd01a
Hwang Seong Jin <tjdwlsl1021@gmail.com>
2020-08-14 07:45:26 +0900

    공부 - AsyncTask, Coroutine2

--- app/src/main/java/com/example/myapplication/Coroutine.kt
+++ app/src/main/java/com/example/myapplication/Coroutine.kt
@@ -32,3 +32,81 @@ GlobalScope.launch(coroutineContext) {
     hideProgress()
     refreshUI(accountList)
 }
+
+
+///===== onCancle을 처리해야하는 경우
+
+// AsyncTask
+private inner class DeleteContactsTask(private val mAccountItem: MyAccountItem) :
+    AsyncTask<Void, Int, Void>() {
+    override fun onPreExecute() {
+        showProgress(R.string.deleting) {
+            cancel(true)
+        }
+    }
+
+    override fun onCancelled(result: Void?) {
+        hideProgress()
+        showToast(R.string.toast_msg_delete_canceled)
+        refreshUI()
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        val deletedCount = values[0]!!
+        increaseProgress(deletedCount)
+    }
+
+    override fun doInBackground(vararg params: Void): Void? {
+        ContactsDBHelper.deleteAllContacts(mAccount) { deletedCount ->
+            publishProgress(deletedCount)
+        }
+
+        ContactsDBHelper.deleteContacts(
+            account = mAccountItem.account,
+            deletedCallback = { deletedCount -> publishProgress(deletedCount) },
+            tackCancelWatcher = { return isCancelled() }
+        )
+        return null
+    }
+
+    override fun onPostExecute(result: Void?) {
+        if (isFinishing) return
+
+        hideProgress()
+        showToast(R.string.toast_msg_delete_success)
+        refreshUI()
+    }
+}
+
+// Coroutine
+GlobalScope.launch(coroutineContext) {
+    val job = launch(Dispatchers.Default) {
+        ContactsDBHelper.deleteContacts(
+            account = myAccount.account,
+            progressListener = { deletedCount ->
+                launch(coroutineContext) {}
+                increaseProgress(deletedCount)
+            }
+    },
+    taskCancelWatcher = {
+        isActive == false )
+        )
+    }
+
+    showProgress(R.string.deleting) {
+        job.cancel()
+    }
+
+    job.join()
+
+    if (isFinishing) return@launch
+    hideProgress()
+
+    if (job.isCancelled) {
+        showToast(R.string.toast_msg_delete_canceled)
+    } else {
+        showToast(R.string.toast_msg_delete_success)
+    }
+    refreshUI()
+}
+
